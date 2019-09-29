f1442b9155b7aed4e450ab1c2e35132d5c9c8eaf
Yacine <Yacinemangaka@gmail.com>
2019-09-29 14:40:45 +0100

    New method with Classes and ES6

--- script.js
+++ script.js
@@ -1,161 +1,120 @@
-const buttons = document.querySelector(".container");
-buttons.addEventListener("click", read);
-
-const array = [];
-array["num1"] = 0;
-array["num2"] = 0;
-array["op"] = "+";
-
-let result = "0";
-
-function read(e) {
-  let current = document.querySelector(".current-result");
-  let previous = document.querySelector(".previous-result");
-  value_clicked = e.target.innerText;
-  if (
-    (isOperation(value_clicked) &&
-      isOperation(previous.textContent.slice(-1))) ||
-    e.target.tagName.toLowerCase() != "button" ||
-    (isPoint(value_clicked) && isPoint(previous.textContent.slice(-1)))
-  ) {
-    return 0;
+class Calculator {
+  constructor(currentOperandTextElement, previousOperandTextElement) {
+    this.currentOperandTextElement = currentOperandTextElement;
+    this.previousOperandTextElement = previousOperandTextElement;
+    this.result = 0;
+    this.clearAll();
   }
 
-  if (current.innerText == 0) {
-    current.innerText = "";
+  clearAll() {
+    this.currentOperand = "";
+    this.previousOperand = "";
+    this.op = undefined;
   }
 
-  if (value_clicked == "AC") {
-    current.innerText = "0";
-    previous.innerText = "";
-    array["num1"] = 0;
-    array["num2"] = 0;
-    array["op"] = "+";
-    result = "0";
-  } else if (value_clicked == "DEL") {
-    if (previous.innerText == "") {
-      current.innerText = "0";
-      array["num1"] = 0;
-      array["num2"] = 0;
-      array["op"] = "+";
-      result = "0";
-      return 0;
+  equal() {
+    if (this.calculate() && this.previousOperand != "") {
+      this.result = this.calculate();
+      this.currentOperand = this.result;
+      this.previousOperand = "";
     }
-    if (
-      isOperation(previous.innerText.slice(-1)) ||
-      isPoint(previous.innerText.slice(-1))
-    ) {
-      previous.innerText = previous.innerText.slice(0, -1);
-      array["num1"] = Number(previous.innerText);
-      array["num2"] = 0;
-      array["op"] = "+";
-      console.log(array);
-      return 0;
+  }
+
+  delete() {
+    console.log("delte");
+    this.currentOperand = this.currentOperandTextElement.innerText.slice(0, -1);
+  }
+  appendNumber(number) {
+    if (number == "." && this.currentOperand.includes(".")) return;
+    this.currentOperand += number.toString();
+  }
+
+  operation(op) {
+    if (currentOperandTextElement.innerText == "") {
+      return;
+    }
+    this.currentOperand = "";
+    this.result = this.calculate();
+    this.op = op;
+    if (this.previousOperand != "") {
+      this.previousOperand = `${this.result} ${this.op}`;
     } else {
-      previous.innerText = previous.innerText.slice(0, -1);
-      array["num1"] = removeLastDigit(array["num1"]);
+      this.previousOperand = `${this.currentOperandTextElement.innerText} ${this.op}`;
     }
-  } else if (isOperation(value_clicked)) {
-    //verificateOperation(current.innerText);
+    /*console.log(this.currentOperand);
+    console.log(this.previousOperand);
+    console.log(this.result);*/
+  }
+  calculate() {
+    let num1 = parseFloat(this.currentOperandTextElement.innerText);
+    let num2 = parseFloat(this.previousOperandTextElement.innerText);
+    switch (this.op) {
+      case "+":
+        console.log("+");
+        return num1 + num2;
+        break;
+      case "-":
+        console.log("-");
+        return num2 - num1;
+        break;
+      case "*":
+        console.log("*");
+        return num1 * num2;
+        break;
+      case "÷":
+        console.log("/");
+        return num2 / num1;
+        break;
 
-    array["num2"] = calculate(array);
-    array["num1"] = 0;
-    array["op"] = value_clicked;
-    previous.innerText = array["num2"] + " " + value_clicked;
-    result = array["num2"];
-
-    /*array["num1"] = Number(current.innerText);
-    array["num2"] = calculate(array);
-    previous.innerText += current.innerText + " " + value_clicked;
-    current.innerText = array["num2"];
-    array["op"] = value_clicked;
-    console.log(array["num2"]);*/
-  } else if (value_clicked == "=") {
-    if (!isOperation(previous.innerText)) {
-      array["num1"] = calculate(array);
-      array["num2"] = 0;
-      result = array["num1"];
-      previous.innerText = "";
-    } else {
-      return 0;
+      default:
+        return this.result;
+        break;
     }
-  } else {
-    if (previous.innerText == "") {
-      current.innerText = "0";
-      array["num1"] = 0;
-      array["num2"] = 0;
-      array["op"] = "+";
-      result = "0";
-    }
-    let temp;
-    if (isPoint(previous.innerText)) {
-      temp = array["num1"] + "." + value_clicked;
-    } else {
-      temp = array["num1"] + value_clicked;
-    }
-    //console.log(temp.slice(-1));
-    //console.log("temp " + temp);
-    //console.log(Number("5.0"));
-    array["num1"] = parseFloat(temp);
-    //console.log("num " + array["num1"]);
-    previous.innerText += " " + value_clicked;
-    result = calculate(array);
   }
-  console.log(array);
-  current.innerText = result;
-}
-
-function calculate(array) {
-  switch (array["op"]) {
-    case "+":
-      return array["num2"] + array["num1"];
-      break;
-    case "-":
-      return array["num2"] - array["num1"];
-      break;
-    case "*":
-      return array["num2"] * array["num1"];
-      break;
-    case "÷":
-      return array["num2"] / array["num1"];
-      break;
-
-    default:
-      return array["num2"];
-      break;
+  updateDisplay() {
+    this.currentOperandTextElement.innerText = this.currentOperand;
+    this.previousOperandTextElement.innerText = this.previousOperand;
   }
 }
 
-function verificateOperation(value) {
-  if (value == ".") {
-    return true;
-  } else {
-    return false;
-  }
-}
+const operationButtons = document.querySelectorAll("[operation-button]");
+const numberButtons = document.querySelectorAll("[number-button]");
+const deleteButton = document.querySelector("[delete-button]");
+const clearAllButton = document.querySelector("[clear-all-button]");
+const equalButton = document.querySelector("[equal-button]");
+const previousOperandTextElement = document.querySelector("[previous-operand]");
+const currentOperandTextElement = document.querySelector("[current-operand]");
 
-function removeLastDigit(num) {
-  num = String(num).slice(0, -1);
-  num = Number(num);
-  return num;
-}
+const calculator = new Calculator(
+  currentOperandTextElement,
+  previousOperandTextElement
+);
 
-function isOperation(value) {
-  if (value == "+" || value == "-" || value == "*" || value == "÷") {
-    return true;
-  }
-  return false;
-}
+numberButtons.forEach(button => {
+  button.addEventListener("click", () => {
+    calculator.appendNumber(button.innerText);
+    calculator.updateDisplay();
+  });
+});
 
-function isPoint(string) {
-  if (string.slice(-1) == ".") {
-    return true;
-  }
-  return false;
-}
-function isEqual(string) {
-  if (string.slice(-1) == "=") {
-    return true;
-  }
-  return false;
-}
+operationButtons.forEach(button => {
+  button.addEventListener("click", () => {
+    calculator.operation(button.innerText);
+    calculator.updateDisplay();
+  });
+});
+
+clearAllButton.addEventListener("click", () => {
+  calculator.clearAll();
+  calculator.updateDisplay();
+});
+
+deleteButton.addEventListener("click", () => {
+  calculator.delete();
+  calculator.updateDisplay();
+});
+
+equalButton.addEventListener("click", () => {
+  calculator.equal();
+  calculator.updateDisplay();
+});
